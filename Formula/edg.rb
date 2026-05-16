865684ad4ba3ce17760e8d9d2b594134fc4cfe2e
codingconcepts <hello@robreid.io>
2026-05-16 07:57:18 +0100

    feat: Add edg tap

---
+++ Formula/edg.rb
@@ -0,0 +1,36 @@
+class Edg < Formula
+  desc "Realistic test data generator"
+  homepage "https://github.com/codingconcepts/edg-releases"
+  version "__VERSION__"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/codingconcepts/edg-releases/releases/download/v#{version}/edg-darwin-arm64"
+      sha256 "__SHA256_DARWIN_ARM64__"
+    end
+    on_intel do
+      url "https://github.com/codingconcepts/edg-releases/releases/download/v#{version}/edg-darwin-amd64"
+      sha256 "__SHA256_DARWIN_AMD64__"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/codingconcepts/edg-releases/releases/download/v#{version}/edg-linux-arm64"
+      sha256 "__SHA256_LINUX_ARM64__"
+    end
+    on_intel do
+      url "https://github.com/codingconcepts/edg-releases/releases/download/v#{version}/edg-linux-amd64"
+      sha256 "__SHA256_LINUX_AMD64__"
+    end
+  end
+
+  def install
+    bin.install stable.url.split("/").last => "edg"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/edg version")
+  end
+end
